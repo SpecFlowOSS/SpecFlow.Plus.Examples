3adaa5313ed240192a2abaea662f09f57f8dc32f
Christoph-Majcen <christoph.majcen@techtalk.at>
2017-03-09 14:13:09 +0100

    Added Excel Example

---
+++ ExcelExample/ExcelExample/Steps/CalculatorMultiplyFeatureSteps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TestCalculator;
+
+namespace ExcelExample.Steps
+{
+    [Binding]
+    public class CalculatorMultiplyFeatureSteps
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorMultiplyFeatureSteps(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [When(@"I press multiply")]
+        public void WhenIPressMultiply()
+        {
+            _calculator.Multiply();
+        }
+    }
+}
---
+++ ExcelExample/Readme.md
@@ -0,0 +1,9 @@
+# Excel Example
+This example demonstrates the three usecases of using SpecFlow+ Excel.
+* Defining full Features in Excel (Add Feature)
+* Defining all examples in Excel (Subtract Feature)
+* Defining only a part of the used examples in Excel (Multiply Feature)
+
+Ofcourse it is als o possible to use non Excel Features alongside Excel Features (Divide Feature)
+
+ 
