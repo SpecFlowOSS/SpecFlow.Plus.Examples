c7fc1da3076b0db0cdacedfa6253c11f8d4a79aa
Christoph-Majcen <christoph.majcen@techtalk.at>
2017-03-13 16:24:19 +0100

    Custom Deployment Steps Project (#2)
    * Added Custom Deployment Steps Example
    * Cleanup SrProfile and unnecessary packages

---
+++ CustomDeploymentSteps/CustomDeploymentSteps/StartWinAppDeploymentStep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using TechTalk.SpecRun.Framework;
+
+namespace CustomDeploymentSteps
+{
+    public class StartWinAppDeploymentStep : IDeploymentTransformationStep
+    {
+        private Process _winApp;
+
+        public void Apply(IDeploymentContext deploymentContext)
+        {
+            var pathToWinApp = deploymentContext.CustomData[this] as string;
+            if (pathToWinApp != null)
+            {
+                _winApp = Process.Start(pathToWinApp);
+            }
+        }
+
+        public void Restore(IDeploymentContext deploymentContext)
+        {
+            _winApp.Kill();
+        }
+    }
+}
