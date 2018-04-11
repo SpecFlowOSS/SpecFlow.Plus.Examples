88b448341c447639fad53d5b1eb63492f7b9eb11
Andreas Willich <andreas.willich@techtalk.at>
2018-04-11 09:52:18 +0200

    add example

---
+++ VSTest_UnifiedResults/VSTest_UnifiedResults/Bindings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace VSTest_UnifiedResults
+{
+    [Binding]
+    class Bindings
+    {
+        private static int _runCounter;
+
+        [Given(@"a always working scenario")]
+        public void GivenAAlwaysWorkingScenario()
+        {
+
+        }
+
+        [Given(@"a scenario that only works the second time")]
+        public void GivenAScenarioThatOnlyWorksTheSecondTime()
+        {
+            _runCounter++;
+
+            if (_runCounter == 1)
+            {
+                Assert.Fail("This scenario doesn't work the first time it runs");
+            }
+        }
+
+    }
+}
