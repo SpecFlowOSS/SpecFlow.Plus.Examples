50d5c88b5e03a75e56b415ac2d94aeff86869f54
David Eiwen <david-e-winlive@live.at>
2019-12-05 13:14:00 +0100

    add new asp.net core project to SeleniumWebTest

---
+++ SeleniumWebTest/TestApplication.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplication.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int summandOne, int summandTwo)
+        {
+            return View(summandOne + summandTwo);
+        }
+
+    }
+}
