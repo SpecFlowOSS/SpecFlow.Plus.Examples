d30cad33147a835887eec8186a7ba3ef93958d15
Andreas Willich <andreas@willich.io>
2020-09-07 11:34:47 +0200

    Sample to access the target name in your APIs

---
+++ AccessTargetName/AccessTargetName/Features/Access Target Name.feature
@@ -0,0 +1,5 @@
+﻿Feature: Access Target Name
+
+Scenario: Access Target Name via Environment variable
+
+	Given the target name is provided as environment variable
---
+++ AccessTargetName/AccessTargetName/Steps/Binding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AccessTargetName.Steps
+{
+    [Binding]
+    public class Binding
+    {
+        [Given(@"the target name is provided as environment variable")]
+        public void GivenTheTargetNameIsProvidedAsEnvironmentVariable()
+        {
+            var targetName = Environment.GetEnvironmentVariable("RUNNER_TARGET");
+
+            Console.WriteLine(targetName);
+        }
+
+    }
+}
