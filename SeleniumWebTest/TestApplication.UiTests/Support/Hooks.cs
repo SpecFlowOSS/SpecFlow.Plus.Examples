436e9427756aba6bb5c6a4f329575632e4316691
Alexandra Horvath-Fung <a.horvath-fung@tricentis.com>
2021-02-04 10:39:36 +0100

    Add hooks to start/stop kestrel

---
+++ SeleniumWebTest/TestApplication.UiTests/Support/Hooks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using TechTalk.SpecFlow;
+using TestApplication.UiTests.Drivers;
+using TestApplication.Web;
+
+namespace TestApplication.UiTests.Support
+{
+    [Binding]
+    public class Hooks
+    {
+        private static IHost _build;
+
+        [BeforeTestRun]
+        public static void StartKestrel(ConfigurationDriver configurationDriver)
+        {
+            _build = CreateHostBuilder(configurationDriver).Build();
+            _build.StartAsync();
+        }
+
+        [AfterTestRun]
+        public static void StopKestrel()
+        {
+            _build.StopAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(ConfigurationDriver configurationDriver)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel()
+                        .UseStartup<Startup>()
+                        .UseUrls(configurationDriver.SeleniumBaseUrl);
+                });
+        }
+    }
+
+
+}
