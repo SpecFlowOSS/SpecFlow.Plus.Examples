e5100957d4183de755f4835ad5db6684726b279b
Alexandra Horváth-Fung <alexandrafung@users.noreply.github.com>
2021-02-05 09:29:06 +0100

    Fix selenium webtest example (#24)
    * Add hooks to start/stop kestrel
    
    * fix link in the readme
    
    * Fix: Copy the report template to the output directory
    
    * Stop kestrel after test run
    
    * Use http instead of https on localhost - self signed cert error in the browsers
    
    * Rename field: _build -> _host to match the type of the object.
    
    Co-authored-by: Alexandra Horvath-Fung <a.horvath-fung@tricentis.com>

---
+++ SeleniumWebTest/TestApplication.UiTests/Support/Hooks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using TechTalk.SpecFlow;
+using TestApplication.UiTests.Drivers;
+using TestApplication.Web;
+
+namespace TestApplication.UiTests.Support
+{
+    [Binding]
+    public class Hooks
+    {
+        private static IHost _host;
+
+        [BeforeTestRun]
+        public static void StartKestrel(ConfigurationDriver configurationDriver)
+        {
+            _host = CreateHostBuilder(configurationDriver).Build();
+            _host.StartAsync();
+        }
+
+        [AfterTestRun]
+        public static void StopKestrel()
+        {
+            _host.Dispose();
+        }
+
+        private static IHostBuilder CreateHostBuilder(ConfigurationDriver configurationDriver)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel()
+                        .UseStartup<Startup>()
+                        .UseUrls(configurationDriver.SeleniumBaseUrl);
+                });
+        }
+    }
+
+
+}
